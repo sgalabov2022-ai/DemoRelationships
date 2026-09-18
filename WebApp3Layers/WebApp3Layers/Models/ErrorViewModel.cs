d95edae96c5d8fa50e643ac0c9bcb61a7dc7454d
sgalabov2022-ai <s.galabov2022@pmg-pernik.com>
2026-09-18 00:21:16 -0700

    created WebApp3Layers

---
+++ WebApp3Layers/WebApp3Layers/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WebApp3Layers.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
